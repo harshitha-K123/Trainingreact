d530ac5d1b1c8de4a23b7b1f510417e01dbf2164
harshitha.harshi <harshithaharshi.051999@gmail.com>
2022-10-19 19:57:41 +0530

    new1 commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
+import React, { useState } from 'react';
 import './App.css';
+//import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 
-
-
 function App() {
+  const [mode, setMode] = useState('light');// Whether dark mode is enable is or not
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark')
+      document.body.style.background = '#042743'
+    }
+    else {
+      setMode('light')
+      document.body.style.background = 'white'
+    }
+  }
   return (
     <>
-   
-  <Navbar title= "TextUtils$"/>
-  <div className="container my-3">
-  <TextForm heading="Enter the text to analyze"/>
-  </div>
-  </>
+
+      <Navbar title="TextUtils$" mode={mode} toggleMode={toggleMode} />
+      <div className="container my-3">
+        <TextForm heading="Enter the text to analyze below" mode={mode} />
+        {/*<About/>*/}
+      </div>
+    </>
   );
 }
 
-
-
 export default App;
+
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,43 +1,48 @@
-import React, {useState} from "react"
+import React, { useState } from "react"
 
 export default function TextForm(props) {
-  const handleUpClick = ()=>{
-  let newText = text.toUpperCase();
+  const handleUpClick = () => {
+    let newText = text.toUpperCase();
     setText(newText)
   }
-  const handleLoClick = ()=>{
+  const handleLoClick = () => {
     let newText = text.toLowerCase();
-      setText(newText)
-    }
-    const handleClearClick = ()=>{
-      let newText = '';
-        setText(newText)
-      }
-
-    const handleOnChange = (event)=>{
-      //console.log("On Change")
-      setText(event.target.value)
+    setText(newText)
   }
-  const[text, setText]=useState ('');
+  const handleClearClick = () => {
+    let newText = '';
+    setText(newText)
+  }
+  const handleExtraSpace = () => {
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "))
+  }
+
+  const handleOnChange = (event) => {
+    //console.log("On Change")
+    setText(event.target.value)
+  }
+  const [text, setText] = useState('');
   return (
     <>
-    <div className="container">
-    <h1>{props.heading}</h1>
-<div className="mb-3">
- 
-  <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="3"></textarea>
-</div>
-<button className="btn btn-primary mx-1" onClick={handleUpClick}>convert to Uppercase</button>
-<button className="btn btn-primary mx-1" onClick={handleLoClick}>convert to Lowercase</button>
-<button className="btn btn-primary" onClick={handleClearClick}>Clear text</button>
-    </div>
-    <div className="container my-3">
-      <h1>Text Summary</h1>
-      <p>{text.split(" ").length} Words and {text.length} Characters</p>
-      <p>{0.008 * text.split(" ").length} Minutes Read</p>
-      <h2>Preview</h2>
-      <p>{text}</p>
-    </div>
+      <div className="container" style={{ color: props.mode === 'dark' ? 'white' : '#042743' }}>
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+
+          <textarea className="form-control" value={text} onChange={handleOnChange} style={{ backgroundColor: props.mode === 'dark' ? 'grey' : 'light', color: props.mode === 'dark' ? 'white' : '#042743' }} id="myBox" rows="3"></textarea>
+        </div>
+        <button className="btn btn-primary mx-1" onClick={handleUpClick}>convert to Uppercase</button>
+        <button className="btn btn-primary mx-1" onClick={handleLoClick}>convert to Lowercase</button>
+        <button className="btn btn-primary mx-1" onClick={handleClearClick}>Clear text</button>
+        <button className="btn btn-primary" onClick={handleExtraSpace}>Clear extra space</button>
+      </div>
+      <div className="container my-3" style={{ color: props.mode === 'dark' ? 'white' : '#042743' }}>
+        <h1>Text Summary</h1>
+        <p>{text.split(" ").length} Words and {text.length} Characters</p>
+        <p>{0.008 * text.split(" ").length} Minutes Read</p>
+        <h2>Preview</h2>
+        <p>{text.length > 0 ? text : "Enter something in the textbox above to preview it here"}</p>
+      </div>
     </>
   )
 }
